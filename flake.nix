670996f49010fd1c1c2139c6af85edb5d371e47c
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-09-14 20:09:04 +0700

    initiating project

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          typescript-language-server
+          nodejs_24
+        ];
+
+        shellHook = ''
+          exec zsh
+        '';
+      };
+  };
+}
